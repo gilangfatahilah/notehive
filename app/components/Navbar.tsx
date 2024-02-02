829ffe5f535fda054e7ea6dcf30c7915325eb2b1
Gilang Fatahilah Akbar <gilangf010@gmail.com>
2024-02-02 15:55:11 +0700

    style : "Change the main theme"

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = async () => {
     <nav className="border-b bg-background h-[10vh] flex items-center">
       <div className="container flex items-center justify-between">
         <Link href="/">
-          <h1 className="font-bold text-3xl">NoteHive.</h1>
+          <h1 className="font-bold text-3xl text-primary">NoteHive.</h1>
         </Link>
 
         <div className="flex items-center gap-x-5">
--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import { RegisterLink } from "@kinde-oss/kinde-auth-nextjs/server";
 
 export default function Home() {
   return (
-    <section className="flex items-center justify-center bg-background h-[98vh]">
+    <section className="flex items-center justify-center bg-background h-[90vh]">
       <div className="relative items-center w-full px-5 py-12 mx-auto lg:px-16 max-w-7xl md:px-12">
-        <div className="max-w-3xl mx-auto text-center">
+        <div className="max-w-6xl mx-auto text-center">
           <div>
             <span className="w-auto px-6 py-3 rounded-full bg-secondary">
               <span className="text-sm font-medium text-primary">
@@ -13,14 +13,14 @@ export default function Home() {
               </span>
             </span>
 
-            <h1 className="mt-8 text-3xl font-extrabold tracking-tight lg:text-6xl">
-              Create Notes with ease.
+            <h1 className="mt-8 text-gradient text-3xl font-extrabold tracking-tight lg:text-7xl">
+              Create notes with ease.
             </h1>
-            <p className="max-w-xl mx-auto mt-8 text-base lg:text-xl text-secondary-foreground">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic
-              saepe harum, corporis molestias facilis libero soluta nostrum
-              nesciunt ea maiores, eum distinctio provident voluptates, alias
-              eligendi minus quasi assumenda aspernatur nam obcaecati.
+            <p className="max-w-2xl mx-auto mt-8 text-base lg:text-xl text-secondary-foreground">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ea
+              unde perspiciatis error iusto at natus voluptatem optio.
+              Accusantium eaque harum quam sequi totam quas voluptate facilis
+              architecto?
             </p>
           </div>
 
