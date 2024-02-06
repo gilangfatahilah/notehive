f2b88214124219b6e304003ea0c407a73c18a0f6
Gilang Fatahilah Akbar <gilangf010@gmail.com>
2024-02-06 13:43:14 +0700

    feat : "setup database"

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,62 @@
 import React, { ReactNode } from "react";
 import { DashboardNav } from "../components/DashboardNav";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+import prisma from "../lib/db";
 
-const DashboardLayout = ({ children }: { children: ReactNode }) => {
+interface User {
+  email: string;
+  id: string;
+  firstName: string | undefined | null;
+  lastName: string | undefined | null;
+  profileImage: string | undefined | null;
+}
+
+const getData = async ({
+  email,
+  id,
+  firstName,
+  lastName,
+  profileImage,
+}: User) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: id,
+    },
+    select: {
+      id: true,
+      stripeCustomerId: true,
+    },
+  });
+
+  if (!user) {
+    const name = `${firstName ?? ""} ${lastName ?? ""}`;
+
+    await prisma.user.create({
+      data: {
+        id: id,
+        email: email,
+        name: name,
+      },
+    });
+  }
+};
+
+const DashboardLayout = async ({ children }: { children: ReactNode }) => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user) {
+    return redirect("/");
+  }
+  await getData({
+    email: user.email as string,
+    firstName: user.given_name as string,
+    id: user.id as string,
+    lastName: user.family_name as string,
+    profileImage: user.picture as string,
+  });
+
   return (
     <div className="flex flex-col space-y-6 mt-10 ">
       <div className="container grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
