50cb003203a61195765513ef12bc80b17f3cd25c
Gilang Fatahilah Akbar <gilangf010@gmail.com>
2024-02-10 15:32:08 +0700

    "feat : "Add responsiveness for navbar""

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,6 +7,14 @@ import {
 } from "@kinde-oss/kinde-auth-nextjs/components";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { UserNav } from "./UserNav";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+import { LogIn, Menu, UserPlus } from "lucide-react";
 
 const Navbar = async () => {
   const { isAuthenticated, getUser } = getKindeServerSession();
@@ -31,14 +39,45 @@ const Navbar = async () => {
               image={user?.picture as string}
             />
           ) : (
-            <div className="flex items-center gap-x-5">
-              <LoginLink>
-                <Button>Sign In</Button>
-              </LoginLink>
-              <RegisterLink>
-                <Button variant={"secondary"}>Sign Up</Button>
-              </RegisterLink>
-            </div>
+            <>
+              <div className="hidden md:flex items-center gap-x-5">
+                <LoginLink>
+                  <Button>Sign In</Button>
+                </LoginLink>
+                <RegisterLink>
+                  <Button variant={"secondary"}>Sign Up</Button>
+                </RegisterLink>
+              </div>
+
+              <div className="block md:hidden">
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant={"outline"} className="px-2">
+                      <Menu className="w-6 h-6 text-primary" />
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    <DropdownMenuItem asChild>
+                      <LoginLink className="w-full flex justify-between items-center cursor-pointer">
+                        Sign In
+                        <span>
+                          <LogIn className="w-4 h-4" />
+                        </span>
+                      </LoginLink>
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>
+                      <RegisterLink className="w-full flex justify-between items-center cursor-pointer">
+                        Sign Up
+                        <span>
+                          <UserPlus className="w-4 h-4" />
+                        </span>
+                      </RegisterLink>
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </div>
+            </>
           )}
         </div>
       </div>
